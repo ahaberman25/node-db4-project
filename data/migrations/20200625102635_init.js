d86678b56320fc5db096b25825c3be1e17e25a55
Andrew Haberman <a.haberman25@gmail.com>
2020-06-25 13:10:59 -0400

    fixed database issues with referencing ingredients

--- data/migrations/20200625102635_init.js
+++ data/migrations/20200625102635_init.js
@@ -5,25 +5,26 @@ exports.up = async function(knex) {
         tbl.text("name").notNull().unique()
         tbl.text("description").notNull()
     })
-    await knex.schema.createTable("ingredients", tbl => {
-        tbl.increments("id")
-        tbl.text("name").notNull().unique()
-    })
     await knex.schema.createTable("steps", tbl => {
-        tbl.increments("id")
-        tbl.integer("step_number").notNull()
+        tbl.float("step_number").notNull()
         tbl
             .integer("recipe_id")
             .references("id")
             .inTable("recipes")
             .onDelete("CASCADE")
             .onUpdate("CASCADE")
+        tbl.text("description")
+        tbl.primary(["step_number"])
+    })
+    await knex.schema.createTable("ingredients", tbl => {
+        tbl.increments("id")
+        tbl.text("name").notNull().unique()
     })
     await knex.schema.createTable("recipe_ingredients", tbl => {
         tbl
-            .integer("recipe_id")
-            .references("id")
-            .inTable("recipes")
+            .integer("step")
+            .references("step_number")
+            .inTable("steps")
             .onDelete("CASCADE")
             .onUpdate("CASCADE")
         tbl 
@@ -34,11 +35,12 @@ exports.up = async function(knex) {
             .onUpdate("CASCADE")
         tbl.float("quantity")
     })
+    
 };
 
 exports.down = async function(knex) {
     await knex.schema.dropTableIfExists("recipe_ingredients")
-    await knex.schema.dropTableIfExists("steps")
-    await knex.schema.dropTableIfExists("ingredients")
+    await knex.schema.dropTableIfExists("ingredients")    
+    await knex.schema.dropTableIfExists("steps")    
     await knex.schema.dropTableIfExists("recipes")
 };
--- data/seeds/004-recipe_ingredients.js
+++ data/seeds/004-recipe_ingredients.js
@@ -1,7 +1,9 @@
 
 exports.seed = async function(knex) {
 	await knex("recipe_ingredients").insert([   
-		{ recipe_id: 1, ingredient_id: 1, quantity: "one cup" },
-		{ recipe_id: 1, ingredient_id: 2, quantity: "1/2 cup" },
+		{ step: 1.1, ingredient_id: 1, quantity: "one cup" },
+    { step: 1.2, ingredient_id: 2, quantity: "1/2 cup" },
+    { step: 2.1, ingredient_id: 1, quantity: "one cup" },
+		{ step: 2.2, ingredient_id: 2, quantity: "1/2 cup" },
 	])
 }
